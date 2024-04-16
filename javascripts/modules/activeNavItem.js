4e096c563035f1d526eff77c151469acaeb40a55
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2024-04-16 15:48:44 +0200

    active nav item v4

--- javascripts/modules/activeNavItem.js
+++ javascripts/modules/activeNavItem.js
@@ -3,13 +3,14 @@ function activeNavItem() {
   let path = window.location.pathname;
   let pathArray = path.split('/');
   let currentPath = pathArray[pathArray.length - 1];
+
   
   links.forEach(link => {
     let linkPath = link.getAttribute('href');
     let linkArray = linkPath.split('/');
     let linkCurrentPath = linkArray[linkArray.length - 1];
     let linkWithoutExtension = linkCurrentPath.replace(".html", "");
-    if (linkWithoutExtension === currentPath || linkPath === currentPath || linkWithoutExtension + ".html" === currentPath || linkPath + ".html" === currentPath) {
+    if (linkWithoutExtension === currentPath) {
       link.classList.add('js-nav--item__active');
     }
   });
