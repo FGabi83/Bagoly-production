ca59b4db9a57858ea7cd04199e8c09043ea93dd6
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2024-04-17 14:10:57 +0200

    active nav item v6

--- dist/App.bundle.js
+++ dist/App.bundle.js
@@ -21,7 +21,6 @@ function accordion() {
   }
   button.addEventListener('click', function () {
     content.classList.toggle('js-active');
-    console.log(content);
     toggleText();
   });
 }
@@ -37,36 +36,22 @@ function accordion() {
 /***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 __webpack_require__.r(__webpack_exports__);
-/*function activeNavItem() {
-  let links = document.querySelectorAll('.js-nav--item');
-  let path = window.location.pathname;
-  let pathArray = path.split('/');
-  let currentPath = pathArray[pathArray.length - 1];
-  
-  links.forEach(link => {
-    let linkPath = link.getAttribute('href');
-    let linkArray = linkPath.split('/');
-    let linkCurrentPath = linkArray[linkArray.length - 1];
-    console.log(linkCurrentPath);
-    if (linkCurrentPath === currentPath || linkCurrentPath + ".html" === currentPath) {
-      link.classList.add('js-nav--item__active');
-    }
-  });
-
-  
-}
-
-export default activeNavItem;*/
-
 function activeNavItem() {
-  var currentPath = window.location.pathname;
-  var navLinks = document.querySelectorAll('.js-nav--item');
-  navLinks.forEach(function (link) {
-    var href = link.getAttribute('href');
-    if (href === '/' && currentPath === '/' || href !== '/' && currentPath.startsWith(href.replace('.html', ''))) {
-      link.classList.add('active');
+  var links = document.querySelectorAll('.js-nav--item');
+  var path = window.location.pathname;
+  var pathArray = path.split('/');
+  var currentPath = pathArray[pathArray.length - 1];
+  links.forEach(function (link) {
+    var linkPath = link.getAttribute('href');
+    var linkArray = linkPath.split('/');
+    var linkCurrentPath = linkArray[linkArray.length - 1];
+    var linkWithoutHtml = linkCurrentPath.split('.')[0];
+    if (linkCurrentPath === currentPath || linkWithoutHtml === currentPath) {
+      console.log('ok', 'linkCurrentPath:', linkCurrentPath, 'currentPath:', currentPath, 'linkWithoutHtml:', linkWithoutHtml);
+      link.classList.add('js-nav--item__active');
     } else {
-      link.classList.remove('active');
+      console.log('not ok', 'linkCurrentPath:', linkCurrentPath, 'currentPath:', currentPath, 'linkWithoutHtml:', linkWithoutHtml);
+      link.classList.remove('js-nav--item__active');
     }
   });
 }
--- javascripts/modules/activeNavItem.js
+++ javascripts/modules/activeNavItem.js
@@ -1,37 +1,25 @@
-/*function activeNavItem() {
+function activeNavItem() {
   let links = document.querySelectorAll('.js-nav--item');
   let path = window.location.pathname;
   let pathArray = path.split('/');
   let currentPath = pathArray[pathArray.length - 1];
-  
   links.forEach(link => {
     let linkPath = link.getAttribute('href');
     let linkArray = linkPath.split('/');
     let linkCurrentPath = linkArray[linkArray.length - 1];
-    console.log(linkCurrentPath);
-    if (linkCurrentPath === currentPath || linkCurrentPath + ".html" === currentPath) {
-      link.classList.add('js-nav--item__active');
+    let linkWithoutHtml = linkCurrentPath.split('.')[0];
+    if (linkCurrentPath === currentPath || linkWithoutHtml === currentPath) {
+       console.log('ok', 'linkCurrentPath:', linkCurrentPath, 'currentPath:', currentPath, 'linkWithoutHtml:', linkWithoutHtml);
+       link.classList.add('js-nav--item__active');
+       } else {
+        console.log('not ok', 'linkCurrentPath:', linkCurrentPath, 'currentPath:', currentPath, 'linkWithoutHtml:', linkWithoutHtml);
+       link.classList.remove('js-nav--item__active');
     }
+    
   });
 
   
 }
 
-export default activeNavItem;*/
-
-function activeNavItem() {
-  var currentPath = window.location.pathname;
-  var navLinks = document.querySelectorAll('.js-nav--item');
-
-  navLinks.forEach(function(link) {
-    var href = link.getAttribute('href');
-    if ((href === '/' && currentPath === '/') || 
-      (href !== '/' && currentPath.startsWith(href.replace('.html', '')))) {
-      link.classList.add('active');
-    } else {
-      link.classList.remove('active');
-    }
-  });
-}
-
 export default activeNavItem;
+
