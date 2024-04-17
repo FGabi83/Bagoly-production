967ea35169f2dbbf57ba15ddd5977db959c3d136
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2024-04-17 13:34:23 +0200

    active nave item v5

--- dist/App.bundle.js
+++ dist/App.bundle.js
@@ -37,17 +37,36 @@ function accordion() {
 /***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 __webpack_require__.r(__webpack_exports__);
-function activeNavItem() {
-  var links = document.querySelectorAll('.js-nav--item');
-  var path = window.location.pathname;
-  var pathArray = path.split('/');
-  var currentPath = pathArray[pathArray.length - 1];
-  links.forEach(function (link) {
-    var linkPath = link.getAttribute('href');
-    var linkArray = linkPath.split('/');
-    var linkCurrentPath = linkArray[linkArray.length - 1];
+/*function activeNavItem() {
+  let links = document.querySelectorAll('.js-nav--item');
+  let path = window.location.pathname;
+  let pathArray = path.split('/');
+  let currentPath = pathArray[pathArray.length - 1];
+  
+  links.forEach(link => {
+    let linkPath = link.getAttribute('href');
+    let linkArray = linkPath.split('/');
+    let linkCurrentPath = linkArray[linkArray.length - 1];
+    console.log(linkCurrentPath);
     if (linkCurrentPath === currentPath || linkCurrentPath + ".html" === currentPath) {
       link.classList.add('js-nav--item__active');
+    }
+  });
+
+  
+}
+
+export default activeNavItem;*/
+
+function activeNavItem() {
+  var currentPath = window.location.pathname;
+  var navLinks = document.querySelectorAll('.js-nav--item');
+  navLinks.forEach(function (link) {
+    var href = link.getAttribute('href');
+    if (href === '/' && currentPath === '/' || href !== '/' && currentPath.startsWith(href.replace('.html', ''))) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
     }
   });
 }
--- javascripts/modules/activeNavItem.js
+++ javascripts/modules/activeNavItem.js
@@ -1,16 +1,15 @@
-function activeNavItem() {
+/*function activeNavItem() {
   let links = document.querySelectorAll('.js-nav--item');
   let path = window.location.pathname;
   let pathArray = path.split('/');
   let currentPath = pathArray[pathArray.length - 1];
-
   
   links.forEach(link => {
     let linkPath = link.getAttribute('href');
     let linkArray = linkPath.split('/');
     let linkCurrentPath = linkArray[linkArray.length - 1];
-    let linkWithoutExtension = linkCurrentPath.replace(".html", "");
-    if (linkWithoutExtension === currentPath) {
+    console.log(linkCurrentPath);
+    if (linkCurrentPath === currentPath || linkCurrentPath + ".html" === currentPath) {
       link.classList.add('js-nav--item__active');
     }
   });
@@ -18,5 +17,21 @@ function activeNavItem() {
   
 }
 
-export default activeNavItem;
+export default activeNavItem;*/
 
+function activeNavItem() {
+  var currentPath = window.location.pathname;
+  var navLinks = document.querySelectorAll('.js-nav--item');
+
+  navLinks.forEach(function(link) {
+    var href = link.getAttribute('href');
+    if ((href === '/' && currentPath === '/') || 
+      (href !== '/' && currentPath.startsWith(href.replace('.html', '')))) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+}
+
+export default activeNavItem;
