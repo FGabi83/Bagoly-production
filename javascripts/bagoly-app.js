2d31ae3780b55cbc3706083ca06f5e6576813c23
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2025-09-19 09:53:38 +0200

    belga sör leiras

--- javascripts/bagoly-app.js
+++ javascripts/bagoly-app.js
@@ -1,9 +1,14 @@
+import '../css/style.css'; // without this import webpack doesn't generate the css file
+
 import dropdownMenu from './modules/dropdownMenu';
 import activeNavItem from './modules/activeNavItem';
+
+import showDetails from './modules/showDetails';
 import accordion from './modules/accordion';
 
 
 dropdownMenu();
 activeNavItem();
+showDetails();
 accordion();
 
